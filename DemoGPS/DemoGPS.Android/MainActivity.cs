f5470375061cd015581a3c887051d65bc465a3fc
jesulink2514 <jesus.angulo@outlook.com>
2018-02-15 14:32:34 -0500

    Add project files.

---
+++ DemoGPS/DemoGPS.Android/MainActivity.cs
@@ -0,0 +1,28 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Plugin.Permissions;
+
+namespace DemoGPS.Droid
+{
+    [Activity(Label = "DemoGPS", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            Plugin.CurrentActivity.CrossCurrentActivity.Current.Activity = this;
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
+
---
+++ DemoGPS/DemoGPS/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Xamarin.Forms;
+
+namespace DemoGPS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnGetLoc.Clicked += BtnGetLoc_Clickied;
+        }
+
+        private async void BtnGetLoc_Clickied(object sender, EventArgs e)
+        {
+            await RetriveLocation();
+        }
+
+        public async Task RetriveLocation()
+        {
+            try
+            {
+                var status = await CrossPermissions.Current.CheckPermissionStatusAsync(Permission.Location);
+                if (status != PermissionStatus.Granted)
+                {
+                    if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.Location))
+                    {
+                        await DisplayAlert("Need location", "Gunna need that location", "OK");
+                    }
+
+                    var results = await CrossPermissions.Current.RequestPermissionsAsync(Permission.Location);
+                    //Best practice to always check that the key exists
+                    if (results.ContainsKey(Permission.Location))
+                        status = results[Permission.Location];
+                }
+
+                if (status == PermissionStatus.Granted)
+                {
+                    var position = await CrossGeolocator.Current.GetPositionAsync(TimeSpan.FromMilliseconds(10000));
+                    txtLat.Text = "Latitude: " + position.Latitude;
+                    txtLong.Text = "Longitude: " + position.Longitude;
+                }
+                else if (status != PermissionStatus.Unknown)
+                {
+                    await DisplayAlert("Location Denied", "Can not continue, try again.", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                txtLat.Text = "Error: " + ex;
+                txtLong.Text = "Error: " + ex;
+            }
+        }
+    }
+}
